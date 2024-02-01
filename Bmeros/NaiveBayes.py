1ed354d58e9d6bbcc6fb8dad5317eb2179ff45db
EIRINI KOUMA <koumarinoueirini@icloud.com>
2024-02-01 17:55:18 +0200

    Load my data , calculate the frequency of each word combined them and make the vocabulary

--- Bmeros/NaiveBayes.py
+++ Bmeros/NaiveBayes.py
@@ -25,3 +25,17 @@ def create_vocabulary(word_freq, m, n, k):
 def create_feature_vector(text, vocabulary):
     text_words = set(text.lower().split())
     return [1 if word in text_words else 0 for word in vocabulary]
+
+# Fortosi dedomenwn
+positive_texts = load_texts('C:\\Users\\eirin\\OneDrive - aueb.gr\\Desktop\\ergasiatexniti\\postrain')
+negative_texts = load_texts('C:\\Users\\eirin\\OneDrive - aueb.gr\\Desktop\\ergasiatexniti\\negtrain')
+positive_texts_test = load_texts('C:\\Users\\eirin\\OneDrive - aueb.gr\\Desktop\\ergasiatexniti\\postest')
+negative_texts_test = load_texts('C:\\Users\\eirin\\OneDrive - aueb.gr\\Desktop\\ergasiatexniti\\negtest')
+
+# Dimiourgia suxnotitas lexewn
+positive_freq = create_word_frequency_map(positive_texts)
+negative_freq = create_word_frequency_map(negative_texts)
+
+# Sundiasmos suxnotitwn
+combined_freq = positive_freq + negative_freq
+vocabulary = create_vocabulary(combined_freq, m=500, n=200, k=50)
